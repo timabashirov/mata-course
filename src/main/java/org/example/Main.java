226cba2b0c0b5f1bbdefc1afcaf375de3c12655b
timabashirov <teymur.besirov15@gmail.com>
2022-09-25 18:40:54 +0100

    First git push

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(number(5));
+    }
+
+    public static Integer number(Integer x){
+        return x+=5;
+    }
+
+}
